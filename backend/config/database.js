d93fccd57abe189d5f745d846621908ba2fff999
Rodsinnetwoking <falonidupe@gmail.com>
2026-08-16 21:47:34 +0100

    Update database.js
    Database temporary change

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,18 @@
 module.exports = function (mongoose) {
     mongoose.set('useFindAndModify', false);
 
-    const uri = process.env.CONNECTION_URI;
+    const uri = process.env.CONNECTION_URI || '';
 
-    console.log('--- MongoDB diagnostic ---');
-    console.log('CONNECTION_URI exists:', !!uri);
-    console.log('Starts with mongodb+srv://:', uri?.startsWith('mongodb+srv://'));
-    console.log('Starts with CONNECTION_URI=:', uri?.startsWith('CONNECTION_URI='));
-    console.log('URI length:', uri ? uri.length : 0);
+    console.log('--- MongoDB URI diagnostic ---');
+    console.log('Exists:', !!uri);
+    console.log('Length:', uri.length);
+    console.log('Starts mongodb+srv://:', uri.startsWith('mongodb+srv://'));
+    console.log('Contains @:', uri.includes('@'));
+    console.log('Contains colon after protocol:', uri.includes(':'));
+    console.log('Contains mongodb.net:', uri.includes('mongodb.net'));
+    console.log('Contains whitespace:', /\s/.test(uri));
+    console.log('Contains < or >:', /[<>]/.test(uri));
+    console.log('Contains quotes:', /["']/.test(uri));
 
     mongoose
         .connect(uri, {
