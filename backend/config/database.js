7790311325dc8964219c4a6947fd20c41da0f577
Rodsinnetwoking <falonidupe@gmail.com>
2026-08-16 21:17:26 +0100

    Update database.js
    Temporary change database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,16 @@
 module.exports = function (mongoose) {
     mongoose.set('useFindAndModify', false);
+
+    const uri = process.env.CONNECTION_URI;
+
+    console.log('--- MongoDB diagnostic ---');
+    console.log('CONNECTION_URI exists:', !!uri);
+    console.log('Starts with mongodb+srv://:', uri?.startsWith('mongodb+srv://'));
+    console.log('Starts with CONNECTION_URI=:', uri?.startsWith('CONNECTION_URI='));
+    console.log('URI length:', uri ? uri.length : 0);
+
     mongoose
-        .connect(process.env.CONNECTION_URI, {
+        .connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             dbName: 'test',
@@ -9,5 +18,5 @@ module.exports = function (mongoose) {
         .then(() => {
             console.log('MongoDB Connected…');
         })
-        .catch(err => console.error(err));
+        .catch(err => console.error('MongoDB error:', err));
 };
